4c22baed080be76cfd6cac4d9adb76dd824a8d77
EricM76 <menaericdaniel@gmail.com>
2022-07-18 20:31:27 -0300

    styles login done

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html'
 app.get('/product-detail', (req,res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html' )));
 app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html' )));
 app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html' )));
+app.get('/terms', (req,res) => res.sendFile(path.join(__dirname, 'views', 'terms.html' )));
 
 app.listen(port, () => console.log('Server running in http://localhost:' + port))
